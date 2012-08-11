0d3e4cd0c5bc5679f4200e09d365110df9db10d4
Justin Campbell <justin@justincampbell.me>
2012-08-10 19:03:17 -0600

    Refactor into instance

--- lib/tp.rb
+++ lib/tp.rb
@@ -2,14 +2,36 @@ require "tp/version"
 
 module TP
   class Presenter
-    def self.present(markdown)
-      slides = markdown.split(/^# /).keep_if { |string| string != "" }.map { |string| "# " + string }
+    attr_accessor :markdown
 
+    def initialize(markdown)
+      self.markdown = markdown
+    end
+
+    def present
       slides.each do |slide|
-        print "\e[2J\e[f"
-        puts slide
-        $stdin.gets
+        clear_screen
+        show_slide slide
+        wait_for_enter
       end
+    end
+
+    def slides
+      result = markdown.split /^# /
+      result.keep_if { |string| string != "" }
+      result.map! { |string| "# " + string }
+    end
+
+    def clear_screen
+      print "\e[2J\e[f"
+    end
+
+    def show_slide(slide)
+      puts slide
+    end
+
+    def wait_for_enter
+      $stdin.gets
     end
   end
 end
--- spec/lib/tp_spec.rb
+++ spec/lib/tp_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe TP::Presenter do
-  describe ".present" do
-    subject(:present) { klass.present markdown }
-    let(:markdown) {
-      "# First Slide\n\n* Bullet 1\n* Bullet 2\n\n# Second Slide"
-    }
+  subject(:presenter) { klass.new markdown }
+  let(:markdown) {
+    "# First Slide\n\n* Bullet 1\n* Bullet 2\n\n# Second Slide"
+  }
 
+  describe "#present" do
     it "works" do
-      klass.should_receive(:print).twice
-      klass.should_receive(:puts).twice
+      presenter.should_receive(:print).twice
+      presenter.should_receive(:puts).twice
       $stdin.should_receive(:gets).twice
 
-      present
+      presenter.present
     end
   end
 end
