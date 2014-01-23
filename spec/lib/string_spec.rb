24cf4d5a6ba8b6332c121b581e958420dd0c9737
Justin Campbell <justin@justincampbell.me>
2014-01-23 13:22:41 -0500

    Fix bug if String#wrap is passed 0

--- spec/lib/string_spec.rb
+++ spec/lib/string_spec.rb
@@ -4,7 +4,16 @@ describe String do
   describe "#wrap" do
     let(:string) { "One two three four five" }
 
+    it { string.wrap(0).should == string }
     it { string.wrap(1).should == "One\ntwo\nthree\nfour\nfive" }
     it { string.wrap(10).should == "One two\nthree four\nfive" }
+
+    generative do
+      data(:length) { rand(200) }
+
+      it "wraps at any length" do
+        expect(string.wrap(length)).to be_a(String)
+      end
+    end
   end
 end
